df9174989619b9bedf054200ae5b8313b210d8aa
birlet <teodora.birle@paddypowerbetfair.com>
2023-12-06 14:07:10 +0200

    Test GitHub connection;

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome5!");
+        System.out.printf("Hello and welcome!");
+        System.out.printf("Test test");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
